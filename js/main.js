2e5d2b38f562c1a11ea79f719c412def80a0fa1f
Alberto Dragoni <albertodragoni@yahoo.it>
2020-03-18 16:43:21 +0100

    ricerca ok

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
 $(document).ready(function(){
-    $.ajax({
-        url: 'https://api.themoviedb.org/3/search/movie',
-        data: {
-            api_key: 'cbbcf8d31c832a4d583973d89a0f4bb4',
-            query: 'ritorno al futuro'
-        },
-        method: 'GET',
-        success: function(data){
-            console.log(data);
-            var films = data.results;
-            for (var i = 0; i < films.length; i++) {
-                var film = films[i];
-                console.log(film.title);
+
+    $('button').click(function(){
+        var titoloInserito = $('input').val();
+        $.ajax({
+            url: 'https://api.themoviedb.org/3/search/movie',
+            data: {
+                api_key: 'cbbcf8d31c832a4d583973d89a0f4bb4',
+                query: titoloInserito,
+                language: 'it-IT'
+            },
+            method: 'GET',
+            success: function(data){
+                var films = data.results;
+                for (var i = 0; i < films.length; i++) {
+                    var film = films[i];
+                        console.log(film.title);
+                        console.log(film.original_title);
+                        console.log(film.language);
+                        console.log(film.vote_average);
+                };
+            },
+            error: function(err){
+                alert('error, try again!')
             }
-        },
-        error: function(err){
-            alert('error, try again!')
-        }
-
-
-
+        });
 
 
     });
-
 
 });
