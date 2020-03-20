c4f6fb97a216017a6e97b4bdfe7bd4719de3aee5
Alberto Dragoni <albertodragoni@yahoo.it>
2020-03-20 12:33:04 +0100

    sistemato tasto enter

--- js/main.js
+++ js/main.js
@@ -4,10 +4,24 @@ $(document).ready(function(){
     var source = $('#scheda-template').html();
     var templateFilm = Handlebars.compile(source);
 
-    $('button').click(function(){
+    $('button').click(cercaFilm);                   //al click sul bottone esegui funzione cercaFilm
+    $('input').keypress(function(event){            //se schiacci enter parte la funzione cercaFilm
+        if (event.keyCode == 13) {
+            cercaFilm();
+        };
+    });
+
+    //funzione per trovare numero intero da 1 a 5;
+    function votoInteger(vote) {
+    var votoFilm = Math.ceil(vote / 2);
+    return votoFilm;
+    };
+    //funzione per la ricerca di film;
+    function cercaFilm() {
         var titoloInserito = $('input').val();
+        var apiBaseUrl = 'https://api.themoviedb.org/3';
         $.ajax({
-            url: 'https://api.themoviedb.org/3/search/movie',
+            url: apiBaseUrl + '/search/movie',
             data: {
                 api_key: 'cbbcf8d31c832a4d583973d89a0f4bb4',
                 query: titoloInserito,
@@ -18,22 +32,24 @@ $(document).ready(function(){
                 var films = data.results;
                 for (var i = 0; i < films.length; i++) {
                     var film = films[i];
+                    console.log(film);
                     var vote = votoInteger(film.vote_average);//utilizzo la funzione voteInteger per trovare intero da 1 a 5
                     console.log(vote);
                     var movieTemplate = {
+                        movieCover: film.poster_path,
                         titolo: film.title,
                         titoloOriginale: film.original_title,
                         lingua: film.original_language,
                         voto: vote
                     };
                     $('.stars').hide();
-                    if (vote < 2) {
+                    if (vote > 1 && vote <= 2)  {
                         $('.prima').show();
-                    } else if (vote < 3) {
+                    } else if (vote > 2 && vote <= 3) {
                         $('.prima, .seconda').show();
-                    } else if (vote < 4) {
+                    } else if (vote > 3 && vote <= 4) {
                         $('.prima, .seconda, .terza').show();
-                    } else if (vote < 5) {
+                    } else if (vote > 4 && vote <= 5) {
                         $('.stars').show();
                     };
                     var schedaFilm = templateFilm(movieTemplate);
@@ -45,14 +61,7 @@ $(document).ready(function(){
                 alert('error, try again!')
             }
         });
-
-
-    });
-    //funzione per trovare numero intero da 1 a 5;
-    function votoInteger(vote) {
-    var votoFilm = Math.ceil(vote / 2);
-    return votoFilm;
-};
+    };
 
 
 });
